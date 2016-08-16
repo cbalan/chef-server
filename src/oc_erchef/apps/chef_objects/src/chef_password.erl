1b5299f2494137560321a6ba752592a7d6f499e4
Tom Duffield <tom@chef.io>
2016-08-16 11:34:16 -0500

    force chef_password:verify to fail if salt is null
    Also, run `crypto:hash` in a case statement to prevent any secure data
    from leaking into the logs.
    
    Signed-off-by: Tom Duffield <tom@chef.io>

--- src/oc_erchef/apps/chef_objects/src/chef_password.erl
+++ src/oc_erchef/apps/chef_objects/src/chef_password.erl
@@ -77,6 +77,9 @@ verify(Password, {HashedPass, _, ?DEFAULT_HASH_TYPE}) ->
     % salt value as well.
     {ok, ThisHashedPass} = bcrypt:hashpw(to_str(Password), to_str(HashedPass)),
     slow_compare(ThisHashedPass, to_str(HashedPass));
+verify(_, {_, null, _}) ->
+    % If Salt is null, reject the login.
+    false;
 verify(Password, {HashedPass, Salt, ?MIGRATION_HASH_TYPE}) ->
     InterimPass = sha1(Salt, Password, ec),
     {ok, ThisHashedPass} = bcrypt:hashpw(to_str(InterimPass), to_str(HashedPass)),
@@ -99,9 +102,17 @@ verify(Password, {HashedPass, Salt, _}) ->
     end.
 
 sha1(Salt, Password, osc) ->
-    hexstring(crypto:hash(sha, ["--", Salt, "--", Password, "--"]));
+    hexstring(safe_crypto(["--", Salt, "--", Password, "--"]));
 sha1(Salt, Password, ec) ->
-    hexstring(crypto:hash(sha, [Salt, "--", Password, "--"])).
+    hexstring(safe_crypto([Salt, "--", Password, "--"])).
+
+%% Eat any exceptions so secure data doesn't escape
+safe_crypto(Data) ->
+    try crypto:hash(sha, Data) of
+        Digest -> Digest
+    catch
+        _ -> <<"crypto:hash failure">>
+    end.
 
 hexstring(<<X:160/big-unsigned-integer>>) ->
     lists:flatten(io_lib:format("~40.16.0b", [X])).
@@ -128,4 +139,3 @@ to_str(S) when is_list(S) ->
     S;
 to_str(S) when is_binary(S) ->
     binary_to_list(S).
-
